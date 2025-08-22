115dacc083ba5ed2024373b1798ea933c41e7e15
Cetecom <fra.godoyc@duocuc.cl>
2025-08-22 15:50:19 -0400

    calculadora

--- clase1/ejemplo1.kt
+++ clase1/ejemplo1.kt
@@ -1,34 +1,42 @@
-package clase1
 fun main(){
     println("inicio de programa")
     val A = 5
     val B = 3
 
-    val sum = A+B
-    val res = A-B
-    val mul = A*B
-    val div = A/B
-    val mod = A % B
+    sumar(A, B)
+    sumar(9,6)
+    sumar(4,7)
+    sumar(93, 1)
 
-    println("estoy en suma")
-    println("sum: $sum")
+    restar(A, B)
+    restar(9,6)
+    restar(4,7)
+    restar(93, 1)
 
-    println("estoy en resta")
-    println("res: $res")
+    multi(A, B)
+    multi(9,6)
+    multi(4,7)
+    multi(93, 1)
 
-    println("estoy en multiplicacion")
-    println("mul: $mul")
 
-    println("estoy en division")
-    println("div: $div")
 
-    println("estoy en modulo")
-    println("mod: $mod")
 
 }
 
 fun sumar(A:Int, B:Int){
     print("estoy en el program de sumar - voy a sumar $A con $B = ")
     println("sum: "+(A+B))
+
+}
+
+
+fun restar(A:Int, B:Int){
+    print("estoy en el program de restar - voy a restar $A con $B = ")
+    println("res: "+(A-B))
+
+}
+fun multi(A:Int, B:Int){
+    print("estoy en el program de multi - voy a multiplicar $A con $B = ")
+    println("res: "+(A*B))
 
 }
